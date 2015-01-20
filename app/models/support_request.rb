9c69c2a9d47826411ed8c28aa5524ac41502d89c
Miroslav Suzara <miroslav.suzara@gmail.com>
2015-01-19 19:06:39 -0800

    Added a regular expression into the model to validate emails.

--- app/models/support_request.rb
+++ app/models/support_request.rb
@@ -3,6 +3,14 @@ class SupportRequest < ActiveRecord::Base
   validates :email, presence: {message: "Please submit an email address!"}
 
   before_save :set_keywords
+  
+  validate :check_email_format
+
+  def check_email_format
+    unless email =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+    errors.add(:email, "Please submit a valid email address.")
+    end
+  end
 
   scope :search, -> (search_word) {
     if search_word.present?
